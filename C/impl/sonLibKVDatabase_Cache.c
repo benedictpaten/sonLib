838f4dd30cb453f34d667f1a001d7ea7018a1380
benedict <benedict@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2010-10-06 16:53:50 +0000

    Incomplete - task : speed everything up
    git-svn-id: svn+ssh://hgwdev.cse.ucsc.edu/projects/compbio/svnroot/hausslerlab/recon/branches/2009-09-16/src/sonLib@6875 a8237c23-d22b-0410-ac1a-db40f18c2caa

--- C/impl/sonLibKVDatabase_Cache.c
+++ C/impl/sonLibKVDatabase_Cache.c
@@ -324,6 +324,9 @@ static void *getRecord(stKVDatabase *database, int64_t key) {
 static void *getPartialRecord(stKVDatabase *database, int64_t key,
         int64_t zeroBasedByteOffset, int64_t sizeInBytes, int64_t recordSize) {
     DiskCache *diskCache = database->cache;
+    if(sizeInBytes == 0) {
+        return st_malloc(0);
+    }
     if (diskCache_containsRecord(diskCache, key, zeroBasedByteOffset,
             sizeInBytes)) {
         int64_t i;
