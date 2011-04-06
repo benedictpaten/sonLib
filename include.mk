a245ed75a0e2ceaa16815d95903e8618b5db9399
Mark Diekhans <markd@soe.ucsc.edu>
2011-04-05 22:24:57 -0700

    use mysql_config to get mysql libraries, etc

--- include.mk
+++ include.mk
@@ -49,16 +49,9 @@ else ifneq ($(wildcard /usr/local/include/tcbdb.h),)
 endif
 
 # location of mysql
-ifneq ($(wildcard /usr/include/mysql/mysql.h),)
-    mysqlIncl = -I/usr/include/mysql -DHAVE_MYSQL=1
-ifneq ($(wildcard /usr/lib64/mysql/libmysqlclient.a),)
-    mysqlLibs = /usr/lib64/mysql/libmysqlclient.a -lssl -lcrypt
-else
-    mysqlLibs = /usr/lib/libmysqlclient.a -lssl -lcrypt
-endif
-else ifneq ($(wildcard /usr/local/mysql/include/mysql.h),)
-    mysqlIncl = -I/usr/local/mysql/include -DHAVE_MYSQL=1
-    mysqlLibs = -L/usr/local/mysql/lib -lmysqlclient -lssl -lcrypt
+ifeq ($(shell mysql_config --version >/dev/null 2>&1 && echo ok),ok)
+    mysqlIncl = $(shell mysql_config --include)
+    mysqlLibs = $(shell mysql_config --libs)
 endif
 
 # location of PostgreSQL
