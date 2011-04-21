0108d488b1155333e8e6eda5d44480229cbff7ba
Benedict <benedictpaten@gmail.com>
2011-04-21 10:06:53 -0700

    Disabled mysql support, fixed build

--- kvdbTest.py
+++ kvdbTest.py
@@ -15,10 +15,11 @@ from sonLib.bioio import system
 class TestCase(unittest.TestCase):
     
     def testSonLibKVTokyoCabinet(self):
-        system("sonLib_kvDatabaseTest --type=tokyocabinet --host=kolossus-10 --user=cactus --pass=cactus  --db=cactus")
+        system("sonLib_kvDatabaseTest --type=tokyocabinet")
 
     def testSonLibKVTokyoTyrant(self):
-        system("sonLib_kvDatabaseTest --type=tokyotyrant --host=kolossus-10.kilokluster.ucsc.edu --port=1978 --user=cactus --pass=cactus  --db=cactus")
+        if socket.gethostname() == "hgwdev":
+            system("sonLib_kvDatabaseTest --type=tokyotyrant --host=kolossus-10.kilokluster.ucsc.edu --port=1978 --user=cactus --pass=cactus  --db=cactus")
 
     def testSonLibKVSQLTest(self):
         if socket.gethostname() == "hgwdev":
