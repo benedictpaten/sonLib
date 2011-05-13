a4cbd95c439cc62f40484a530172162aa7655507
Evan Paull <evanpaull@gmail.com>
2011-05-13 16:54:21 -0700

    --

--- C/tests/kt_connect_test.cpp
+++ C/tests/kt_connect_test.cpp
@@ -19,24 +19,61 @@ int main(int argc, char** argv) {
   }
 
   rdb->clear();
+  string value;
 
   rdb->add("1", "123");
+  cerr << "add record error: " << rdb->error().name() << endl;
   rdb->increment("1", 23);
-  // 450 (the existing record was not compatible).
+  //450 (the existing record was not compatible).
   cerr << "increment error: " << rdb->error().name() << endl;
+  rdb->get("1", &value);
+  cout << "key 1 incremented to (should be 146): " << value << endl;
+
+  rdb->clear();
+
+  map<string,string> recs;
+  recs.insert( pair<string,string>("5", "5") );
+  recs.insert( pair<string,string>("6", "10") );
+  recs.insert( pair<string,string>("7", "15") );
+  recs.insert( pair<string,string>("8", "20") );
+  int64_t retVal = rdb->set_bulk(recs, 5, true);
+  cerr << "retval bulk set: " << retVal << endl;
+  cerr << " bulk retval: " << rdb->error().name() << endl;
 
      // retrieve a record
-  string value;
-  if (rdb->get("1", &value)) {
-    cout << "key 1 set to (123 + 23 incr)" << value << endl;
-  } else {
-    cerr << "get error: " << rdb->error().name() << endl;
-  }
+  rdb->get("5", &value);
+  cout << "key 5 set to (should be 5): " << value << endl;
+
+  rdb->get("6", &value);
+  cout << "key 6 set to (should be 10): " << value << endl;
+
+  rdb->get("7", &value);
+  cout << "key 7 set to (should be 15): " << value << endl;
+
+  rdb->get("8", &value);
+  cout << "key 8 set to (should be 20): " << value << endl;
+
+ // C version of increment
+
+    /* 
+ int64_t key = 2;
+ size_t keysize = sizeof(int64_t);
+ int64_t incAmt = 55;
+ int64_t xt = 5;
+ 
+ const char *initialVal = "43";
+ int64_t sizeOfRecord = sizeof(int64_t);
+
+ rdb->add((char *)&key, keysize, initialVal, sizeOfRecord);
+ cerr << "add record error: " << rdb->error().name() << endl;
+ rdb->increment((char *)&key, keysize, incAmt, xt);
+ cerr << "increment record error: " << rdb->error().name() << endl;
 
   // close the database
   if (!rdb->close()) {
     cerr << "close error: " << rdb->error().name() << endl;
   }
+    */
 
   return 0;
 }
