8641d97cf402a3670a0b8e5bf7d7cb6db06ba40c
Glenn Hickey <hickey@soe.ucsc.edu>
2011-09-29 11:10:53 -0700

    tests cope with weights stored as integers (ignore tailing 0s)
    Change-Id: I915f19b05470202debba99a6eb4efc07a47bab71

--- nxnewickTest.py
+++ nxnewickTest.py
@@ -23,9 +23,9 @@ class TestCase(unittest.TestCase):
     
     def testNewickIO(self):
         # feslenstein's own... (http://evolution.genetics.washington.edu/phylip/newicktree.html)
-        tree1 = '((raccoon:19.19959,bear:6.80041):0.84600,((sea_lion:11.99700, seal:12.00300):7.52973,((monkey:100.85930,cat:47.14069):20.59201, weasel:18.87953):2.09460):3.87382,dog:25.46154);'
-        tree2 = '(Bovine:0.69395,(Gibbon:0.36079,(Orang:0.33636,(Gorilla:0.17147,(Chimp:0.19268, Human:0.11927):0.08386):0.06124):0.15057):0.54939,Mouse:1.21460):0.10;'
-        tree3 = '(Bovine:0.69395,(Hylobates:0.36079,(Pongo:0.33636,(G._Gorilla:0.17147, (P._paniscus:0.19268,H._sapiens:0.11927):0.08386):0.06124):0.15057):0.54939, Rodent:1.21460);'
+        tree1 = '((raccoon:19.19959,bear:6.80041):0.846,((sea_lion:11.997, seal:12.003):7.52973,((monkey:100.8593,cat:47.14069):20.59201, weasel:18.87953):2.0946):3.87382,dog:25.46154);'
+        tree2 = '(Bovine:0.69395,(Gibbon:0.36079,(Orang:0.33636,(Gorilla:0.17147,(Chimp:0.19268, Human:0.11927):0.08386):0.06124):0.15057):0.54939,Mouse:1.2146):0.1;'
+        tree3 = '(Bovine:0.69395,(Hylobates:0.36079,(Pongo:0.33636,(G._Gorilla:0.17147, (P._paniscus:0.19268,H._sapiens:0.11927):0.08386):0.06124):0.15057):0.54939, Rodent:1.2146);'
         tree4 = 'A;'
         tree5 = '((A,B),(C,D));'
         tree6 = '(Alpha,Beta,Gamma,Delta,,Epsilon,,,);'
@@ -50,7 +50,7 @@ class TestCase(unittest.TestCase):
             if c not in ws: 
                 result += c
         if result[len(result) -2 ] != ')' and result[0] == '(':
-                result = "(%s);" % result[:len(result) - 2]
+                result = "(%s);" % result[:len(result) - 1]
         return result
     
 if __name__ == '__main__':
