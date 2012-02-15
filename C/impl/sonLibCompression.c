025226089ff069dc78424ef0aecdcd5361b7338e
Benedict <benedictpaten@gmail.com>
2012-02-15 15:39:37 -0800

    Switching to default zlib compression level (was previously 8 if default used, where as zlib level is 6),

--- C/impl/sonLibCompression.c
+++ C/impl/sonLibCompression.c
@@ -21,7 +21,7 @@ void *stCompression_compress(void *data, int64_t sizeInBytes, int64_t *compresse
     uLongf bufferSize = compressBound(sizeInBytes);
     void *buffer = st_malloc(bufferSize);
     if(level == -1) {
-        level = 8;
+        level = Z_DEFAULT_COMPRESSION;
     }
     int32_t i = compress2(buffer, &bufferSize, data, sizeInBytes, level);
     if(i != Z_OK) {
