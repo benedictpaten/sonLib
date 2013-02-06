a023063a028aad50c70a102a8fd7da197bb3cf18
benedictpaten <benedictpaten@gmail.com>
2013-02-05 17:20:42 -0800

    Modifying compression function to deal with buffer overruns

--- C/impl/sonLibCompression.c
+++ C/impl/sonLibCompression.c
@@ -18,19 +18,27 @@
 const char *ST_COMPRESSION_EXCEPTION_ID = "ST_COMPRESSION_EXCEPTION";
 
 void *stCompression_compress(void *data, int64_t sizeInBytes, int64_t *compressedSizeInBytes, int32_t level) {
-    uLongf bufferSize = compressBound(sizeInBytes);
-    void *buffer = st_malloc(bufferSize);
-    if(level == -1) {
+    if (level == -1) {
         level = Z_DEFAULT_COMPRESSION;
     }
-    int32_t i = compress2(buffer, &bufferSize, data, sizeInBytes, level);
-    if(i != Z_OK) {
-        stThrowNew(ST_COMPRESSION_EXCEPTION_ID, "Tried to compress a string of %lld bytes but got the Z_ERROR code %i", (long long)bufferSize, i);
+    uLongf bufferSize = compressBound(sizeInBytes);
+    while (1) {
+        void *buffer = st_malloc(bufferSize);
+        int32_t i = compress2(buffer, &bufferSize, data, sizeInBytes, level);
+        if (i == Z_OK) {
+            void *compressedData = memcpy(st_malloc(bufferSize), buffer, bufferSize);
+            free(buffer);
+            *compressedSizeInBytes = bufferSize;
+            return compressedData;
+        }
+        if (i == Z_BUF_ERROR) {
+            bufferSize *= 2;
+            free(buffer);
+        } else {
+            stThrowNew(ST_COMPRESSION_EXCEPTION_ID,
+                    "Tried to compress a string of %lld bytes but got the Z_ERROR code %i", (long long) bufferSize, i);
+        }
     }
-    void *compressedData = memcpy(st_malloc(bufferSize), buffer, bufferSize);
-    free(buffer);
-    *compressedSizeInBytes = bufferSize;
-    return compressedData;
 }
 
 /*
@@ -38,22 +46,22 @@ void *stCompression_compress(void *data, int64_t sizeInBytes, int64_t *compresse
  * of the decompressed string.
  */
 void *stCompression_decompress(void *compressedData, int64_t compressedSizeInBytes, int64_t *sizeInBytes) {
-    uLongf bufferSize = compressedSizeInBytes*2 + 1; //The one just in case we had zero compressed size!
-    while(1) {
+    uLongf bufferSize = compressedSizeInBytes * 2 + 1; //The one just in case we had zero compressed size!
+    while (1) {
         void *buffer = st_malloc(bufferSize);
         int32_t i = uncompress(buffer, &bufferSize, compressedData, compressedSizeInBytes);
-        if(i == Z_OK) {
+        if (i == Z_OK) {
             void *uncompressedData = memcpy(st_malloc(bufferSize), buffer, bufferSize);
             free(buffer);
             *sizeInBytes = bufferSize;
             return uncompressedData;
-        }
-        else if (i == Z_BUF_ERROR) {
+        } else if (i == Z_BUF_ERROR) {
             bufferSize *= 2;
             free(buffer);
-        }
-        else {
-            stThrowNew(ST_COMPRESSION_EXCEPTION_ID, "Tried to decompress a string of %lld compressed bytes but got the Z_ERROR code %i", (long long)compressedSizeInBytes, i);
+        } else {
+            stThrowNew(ST_COMPRESSION_EXCEPTION_ID,
+                    "Tried to decompress a string of %lld compressed bytes but got the Z_ERROR code %i",
+                    (long long) compressedSizeInBytes, i);
         }
     }
     return compressedData;
